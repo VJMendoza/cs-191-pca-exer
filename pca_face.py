e6777c2888d4c5e0c873e818ecae9ea4d62d2f58
VJ Mendoza <virgiliomendoza05@gmail.com>
2019-04-19 20:43:03 +0800

    Added function to let user input n_components in cmd; added paper

--- pca_face.py
+++ pca_face.py
@@ -6,6 +6,8 @@ from sklearn import svm
 
 import pandas as pd
 import numpy as np
+import getopt
+import sys
 
 
 kernels = ['rbf', 'linear', 'poly', 'sigmoid']
@@ -20,8 +22,8 @@ def classify_data(train_data, test_data, train_label, test_label):
         precision_score(test_label, pred), f1_score(test_label, pred)
 
 
-def decompose_data(train_data, test_data):
-    pca_decomposer = PCA(290)
+def decompose_data(train_data, test_data, components):
+    pca_decomposer = PCA(components)
     pca_decomposer.fit(train_data)
     return pca_decomposer.transform(train_data), \
         pca_decomposer.transform(test_data), \
@@ -40,11 +42,33 @@ def load_data():
     return train_set.data, test_set.data, train_set.target, test_set.target
 
 
+def main(argv):
+    components = 0
+    try:
+        opts, _ = getopt.getopt(
+            argv, 'hc:', ['help', 'components'])
+    except getopt.GetoptError:
+        print('Invalid argument')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('pca_face.py -c <n_components>')
+        elif opt in ('-c', '--components'):
+            components = arg
+
+    return components
+
+
 if __name__ == "__main__":
+    components = int(main(sys.argv[1:]))
     train_data, test_data, train_target, test_target = load_data()
     train_data, test_data = scale_data(train_data, test_data)
-    train_data, test_data, var_cumsum = decompose_data(train_data, test_data)
-    print("Cumulative variance: ", var_cumsum[-10:])
+    train_data, test_data, var_cumsum = decompose_data(
+        train_data, test_data, components)
+
+    print("Cumulative variance with n_components = {0}: {1:.4%}".format(
+        components, var_cumsum[-1]))
 
     acc_res, prec_res, f1_res = classify_data(train_data, test_data,
                                               train_target, test_target)
